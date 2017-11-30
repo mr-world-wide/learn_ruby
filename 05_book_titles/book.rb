13bad86e483ba1a58f6763fe841136e2b42a194e
Josh Cameron <josh@tanda.co>
2017-11-30 09:48:46 +0000

    Ruby exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
 # write your code here
+ attr_accessor :title
+ 
+ def initialize
+ end
+ 
+  def title
+    title_words = @title.split()
+    cap_title = []
+    title_words.each do |word|
+     cap_title << word.capitalize!
+    end
+    
+    @title = cap_title.join(" ")
+ 
+  end
+
+def capitalize(word)
+  word.capitalize!
 end
+
+
+
+end
+ 
